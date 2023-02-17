e48a8022a9fa71fcccf359771a51a2427df02441
agujimcas <agujimcas@alum.us.es>
2023-02-17 12:18:51 +0100

    Viernes 17 Feb

--- indexA.js
+++ indexA.js
@@ -1,6 +1,48 @@
+/*
 var a = 1; b = "1";
 if(a==b){
     console.log("Iguales");
 } else{
     console.log("Distintos");
 }
+*/
+
+function log(msg){
+    console.log(msg);
+}
+
+log("Mi fantastico log");
+
+var numbers = [1,2,3,4];
+
+/*
+a[0] = 1;
+a[1] = 2;
+a[2] = 3;
+a[3] = 4;
+
+a.push(5);
+*/
+
+/*
+for(var i=0;i<numbers.length;i++){
+    log(numbers[i]);
+}
+
+numbers.forEach(log);
+
+numbers.forEach(function(msg){
+    console.log(msg);
+});
+*/
+
+var i = numbers.filter((n)=>{
+    return n > 2;
+}).map((n)=>{
+    return n+1;
+}).reduce((a,n)=>{
+    return n+a;
+});
+
+log(i);
+

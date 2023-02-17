ccf044f60f88fd61057098e384f8ba819cb63de8
ricardovrodri <93518567+ricardovrodri@users.noreply.github.com>
2023-02-17 12:19:57 +0100

    Fin Sesion JavaScript

--- NodeJS_pruebaR/index.js
+++ NodeJS_pruebaR/index.js
@@ -12,5 +12,34 @@ Javascript es un infierno, hay que ser ordenado y responsable con lo que se escr
 
 La guia de referencia que le gusta es MDN JavaScript*/
 
-console.log('hola');
+function log(msg){
+    console.log(msg)
+}
 
+var a = [1,2,3,4];
+
+/*for(var i=0; i<a.length; i++){
+    log(a[i]);
+}*/
+
+a.forEach(log); //Concepto de Callback: Paso una funcion como argumento. 
+
+/* Pasar como argumento de una funcion se puede hacer de mil maneras, podemos directamente
+definir la función en la propia llamada y podemos llamarla sin nombre sería algo así
+
+a.forEach(function (msg){ console.log(msg) })
+
+De hecho en las últimas actualizaciones se han metido el operador => que simplifica
+aun mas la sintaxis de este concepto 
+
+a.forEach((msg) => { console.log(msg) }) */
+
+// Iterador filter
+
+// a.filter((n) => {return n > 2;}).forEach(log); Devuelve 3 y 4
+
+//Iterador Map
+
+// a.map((n) => {return 2*n;}).forEach(log);  Devuelve 2,4,6 y 8
+
+//Iterador reduce

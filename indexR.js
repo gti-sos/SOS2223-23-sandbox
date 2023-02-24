5790d82b26fb5c988044336c5549d350c2dc0da6
ricardovrodri <93518567+ricardovrodri@users.noreply.github.com>
2023-02-24 12:20:03 +0100

    Fin sesion L05

--- indexR.js
+++ indexR.js
@@ -1,3 +1,18 @@
-var cool = require('cool-ascii-faces');
+//Esto son pruebas del lab05, este fichero desaparecera en la rama prinicpal
 
-console.log(cool());
+var cool = require('cool-ascii-faces');
+var express = require('express');
+
+
+var app = express();
+var port = 12345;
+
+app.get('/faces', (req, res) => {
+    res.send(cool());
+    console.log('New request')
+})
+
+app.listen(port, () => {
+    console.log(`Server ready in port ${port}`);
+});
+
